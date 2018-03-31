a9149e5b30bb0cf482b4cdf6c2fd27cb9327d1f3
Blast06 <lacalculadora06@hotmail.com>
2018-03-31 00:12:22 -0400

    relacion de productos y review creada

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,10 +2,18 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     
-    //
+    public function reviews()
+    {
+
+        return $this->hasMany(Review::class);
+
+    }
+
+    
 }
